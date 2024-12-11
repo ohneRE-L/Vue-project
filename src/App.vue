b09986a30eb776f100783216f740ce12cb988666
ohneRE-L <fazilov.p.i.23@gmail.com>
2024-12-11 17:12:34 +0300

    Задание 1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: 'john',
+      surn: 'smit',
+    };
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <main>
+    <div>{{ name }}</div>
+    <div>{{ surn }}</div>
+  </main>
 
   <RouterView />
 </template>
